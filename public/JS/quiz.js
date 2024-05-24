57718315d76472508dbd8bcd841e82cf5458016d
LCarminatti <lucas.csantos@sptech.school>
2024-05-24 14:41:31 -0300

    Quiz v1

---
+++ public/JS/quiz.js
@@ -0,0 +1,163 @@
+const startGameButton = document.querySelector(".start-quiz");
+const questionsContainer = document.querySelector(".questions-container");
+const answersContainer = document.querySelector(".answers-container");
+const questionText = document.querySelector(".question");
+const nextQuestionButton = document.querySelector(".next-question");
+const container = document.querySelector(".container");
+const exitButton = document.querySelector(".exit-button");
+
+startGameButton.addEventListener("click", startGame);
+nextQuestionButton.addEventListener("click", displayNextQuestion);
+
+let currentQuestionIndex = 0;
+let totalCorrect = 0;
+
+function startGame(params) {
+  startGameButton.classList.add("hide");
+  questionsContainer.classList.remove("hide");
+  exitButton.classList.add("hide");
+
+  displayNextQuestion();
+}
+
+function displayNextQuestion(params) {
+  resetState();
+
+  if (questions.length == currentQuestionIndex) {
+    return finishGame();
+  }
+
+  questionText.textContent = questions[currentQuestionIndex].question;
+  questions[currentQuestionIndex].answers.forEach((answers) => {
+    const newAnswer = document.createElement("button");
+    newAnswer.classList.add("button", "answer");
+    newAnswer.textContent = answers.text;
+    if (answers.correct) {
+      newAnswer.dataset.correct = answers.correct;
+    }
+    answersContainer.appendChild(newAnswer);
+
+    newAnswer.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState(params) {
+  while (answersContainer.firstChild) {
+    answersContainer.removeChild(answersContainer.firstChild);
+  }
+
+  document.body.removeAttribute("class");
+  nextQuestionButton.classList.add("hide");
+}
+
+function selectAnswer(event) {
+  const answerClicked = event.target;
+
+  if (answerClicked.dataset.correct) {
+    document.body.classList.add("correct");
+    totalCorrect++;
+  } else {
+    document.body.classList.add("incorrect");
+  }
+
+  document.querySelectorAll(".answer").forEach((button) => {
+    if (button.dataset.correct) {
+      button.classList.add("correct");
+    } else {
+      button.classList.add("incorrect");
+    }
+
+    button.disabled = true;
+  });
+  nextQuestionButton.classList.remove("hide");
+  currentQuestionIndex++;
+}
+
+function finishGame(params) {
+  const totalQuestion = questions.length;
+  const performance = Math.floor((totalCorrect * 100) / totalQuestion);
+
+  let message = "";
+
+  switch (true) {
+    case performance >= 90:
+      message = "Excelente :)";
+      break;
+    case performance >= 70:
+      message = "Muito Bom :)";
+      break;
+    case performance >= 50:
+      message = "Bom";
+      break;
+    default:
+      message = "Pode Melhorar :(";
+      break;
+  }
+
+  questionsContainer.innerHTML = `
+  <p>
+   Você acertou ${totalCorrect} de ${totalQuestion} questões!
+  <span>Resultado: ${message}</span>
+    </p>
+<button onclick=window.location.reload()>Refazer Teste</button>`;
+}
+
+const questions = [
+  {
+    question: "Qual o maior ranking do R6?",
+    answers: [
+      { text: "Master", correct: false },
+      { text: "Master League", correct: false },
+      { text: "Diamante", correct: false },
+      { text: "Campeôes", correct: true },
+    ],
+  },
+  {
+    question:
+      "Quais personagens possuem escudo?",
+    answers: [
+      { text: "Clash - Blitz - Fuze - Montagne", correct: true },
+      { text: "Iana - Ash - Smoke - Lesion", correct: false },
+      { text: "Meluse - Castle - Nokk - Ela", correct: false },
+      { text: "Mute - Mozzie - Mira - Glaz", correct: false },
+    ],
+  },
+  {
+    question: "Quais personagens curam no jogo?",
+    answers: [
+      { text: "Smoke - Grim - Sledge", correct: false },
+      { text: "Recruta - Blitz - Fuze", correct: false },
+      { text: "Ela - Ash - Twitch", correct: false },
+      { text: "Doc - Thunderbird - Finka", correct: true },
+    ],
+  },
+  {
+    question: "Quais personagens dão choque?",
+    answers: [
+      { text: "Lesion - Solis - Zofia", correct: false },
+      { text: "Smoke - Bandit - Sledge - Hibana", correct: false },
+      { text: "Clash - Kaid - Bandit - Twitch", correct: true },
+      { text: "Ela - Flores - Grim - Gridlock", correct: false },
+    ],
+  },
+  {
+    question:
+      "Qual é o persongem grátis?",
+    answers: [
+      { text: "Azami", correct: false },
+      { text: "Recruta", correct: true },
+      { text: "Clash", correct: false },
+      { text: "Bandit", correct: false },
+    ],
+  },
+  {
+    question:
+      "Dos 4 operadores da Spetsnaz, qual deles possui a opção de trocar a mira no meio da rodada?",
+    answers: [
+      { text: "Kapkan", correct: false },
+      { text: "Glaz", correct: true },
+      { text: "Fuze", correct: false },
+      { text: "Tachanka", correct: false },
+    ],
+  },
+];
